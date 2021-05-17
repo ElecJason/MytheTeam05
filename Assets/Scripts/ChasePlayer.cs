414b601b2fbe3fb4c1a5c7b11aae6a6b61260219
Jason Dikken <jasondikken1@gmail.com>
2021-05-17 11:21:12 +0200

    added basic Nav Agent chase
    Added a very basic Nav AI agent, aswell as baked a small begin level.

---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private NavMeshAgent agent;
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
