59c75aa924175a92ac39292b69c6a3dd5d10de73
Jason Dikken <jasondikken1@gmail.com>
2021-05-20 09:39:13 +0200

    added the prototype ending

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("awake");
+        }
+    }
+}
