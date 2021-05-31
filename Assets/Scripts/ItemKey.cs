7fcaf212a221cdf4e3f52fa8c0841ec5aa1b7b7c
Jason Dikken <jasondikken1@gmail.com>
2021-05-31 11:20:13 +0200

    implemanted and reworked key and inventory

--- Assets/Scripts/ItemKey.cs
+++ Assets/Scripts/ItemKey.cs
@@ -5,15 +5,19 @@ using UnityEngine.Events;
 
 public class ItemKey : ItemBase
 {
-    [SerializeField] private UnityEvent GetKey;
+    private GameObject player;
 
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
     public override void OnTriggerEnter(Collider other)
     {
         base.OnTriggerEnter(other);
 
         if (other.gameObject.CompareTag("Player"))
         {
-            GetKey.Invoke();
+            player.GetComponent<Inventory>().PickupKey();
             Destroy(this.gameObject);
             Debug.Log("Key Found");
         }
