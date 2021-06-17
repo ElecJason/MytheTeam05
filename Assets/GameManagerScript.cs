3d7aa7dfc8fb75241c8cf3aad6a60767ba2b5a8f
Jason Dikken <jasondikken1@gmail.com>
2021-06-17 10:53:07 +0200

    added the win and lose screen

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -7,8 +7,15 @@ public class GameManagerScript : MonoBehaviour
 { 
     public void EndGame()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
         Cursor.lockState = CursorLockMode.Confined;
         Debug.LogError("De game is over");
+    }
+
+    public void WinGame()
+    {
+        SceneManager.LoadScene(3);
+        Cursor.lockState = CursorLockMode.Confined;
+        Debug.LogError("Ik won de game");
     }
 }
