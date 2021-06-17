a34a3b1e6224b434b13bb4bc36916ad4e20e9271
ti-jen <tijnkroon@me.com>
2021-06-17 20:43:53 +0200

    post proccing changes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{ 
+    public void EndGame()
+    {
+        SceneManager.LoadScene(3);
+        Cursor.lockState = CursorLockMode.Confined;
+        Debug.LogError("De game is over");
+    }
+}
