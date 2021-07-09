7009150c1dd5d9fb15561961d2b0646d895de394
Jason Dikken <jasondikken1@gmail.com>
2021-07-09 12:04:05 +0200

    fixed error with the startscreens

--- Assets/Scripts/GUI/StartScreenLoader.cs
+++ Assets/Scripts/GUI/StartScreenLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartScreen : MonoBehaviour
+public class StartScreenLoader : MonoBehaviour
 {
     public void PlayGame()
     {
